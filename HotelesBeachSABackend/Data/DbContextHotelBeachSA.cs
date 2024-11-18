b7a622fdee783fea5fb907c3541d897054a166cc
cap1112 <angelv.salazar11@gmail.com>
2024-11-17 22:13:47 -0600

    feet: DBContext and program file init

---
+++ HotelesBeachSABackend/Data/DbContextHotelBeachSA.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelesBeachSABackend.Models;
+
+namespace HotelesBeachSABackend.Data
+{
+    public class DbContextHotelBeachSA : DbContext
+    {
+        public DbContextHotelBeachSA(DbContextOptions<DbContextHotelBeachSA> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ HotelesBeachSABackend/Models/Usuario.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+
+namespace HotelesBeachSABackend.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Cedula { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Tipo_Cedula { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Nombre_Completo { get; set; }
+        
+        [Required]
+        [Column(TypeName = "VARCHAR(20)")]
+        [StringLength(20, ErrorMessage = "El teléfono no puede tener más de 20 caracteres.")]
+        public string Telefono { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(255)")]
+        public string Direccion { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(100)")]
+        [EmailAddress(ErrorMessage = "El formato del correo no es válido.")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(255)")]
+        public string Password { get; set; }
+    }
+}
