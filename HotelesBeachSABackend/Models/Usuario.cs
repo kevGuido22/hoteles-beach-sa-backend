371685903263d8edbfc527113f6134940461643d
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-11-18 00:05:11 -0600

    feat: Usuario model refactiored

--- HotelesBeachSABackend/Models/Usuario.cs
+++ HotelesBeachSABackend/Models/Usuario.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Numerics;
 
 namespace HotelesBeachSABackend.Models
 {
@@ -9,33 +8,27 @@ namespace HotelesBeachSABackend.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
-        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Debe ingresar los digitos de la cedula")]
         public string Cedula { get; set; }
 
-        [Required]
-        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Debe de indicar el tipo de cedula")]
         public string Tipo_Cedula { get; set; }
 
-        [Required]
-        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Debe de ingresar el nombre completo")]
         public string Nombre_Completo { get; set; }
         
-        [Required]
-        [Column(TypeName = "VARCHAR(20)")]
+        [Required(ErrorMessage = "Debe de ingresar el numero de telefono")]
         [StringLength(20, ErrorMessage = "El teléfono no puede tener más de 20 caracteres.")]
         public string Telefono { get; set; }
 
-        [Required]
-        [Column(TypeName = "VARCHAR(255)")]
+        [Required(ErrorMessage = "Debe de ingresar la direccion")]
         public string Direccion { get; set; }
 
-        [Required]
-        [Column(TypeName = "VARCHAR(100)")]
+        [Required(ErrorMessage = "Debe de ingresar el correo electronico")]
         [EmailAddress(ErrorMessage = "El formato del correo no es válido.")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Debe de ingresar la contraseña")]
         [Column(TypeName = "VARCHAR(255)")]
         public string Password { get; set; }
     }
