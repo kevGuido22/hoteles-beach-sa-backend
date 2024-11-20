8b72ae20f458693ce986ba3d1c943c6de0237839
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-11-20 14:04:50 -0600

    feat: RolPermiso response added

---
+++ HotelesBeachSABackend/Models/Custom/RolPermisoResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelesBeachSABackend.Models.Custom
+{
+    public class RolPermisoResponse
+    {
+        public string Rol { get; set; }
+        public List<string> Permisos { get; set; }
+    }
+}
