670517b44a481ef3348f0ee1bd781c09a21a63fb
cap1112 <angelv.salazar11@gmail.com>
2024-11-20 17:10:59 -0600

    feat: index method for Factura added

---
+++ HotelesBeachSABackend/Controllers/FacturaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HotelesBeachSABackend.Data;
+using HotelesBeachSABackend.Models;
+using Microsoft.EntityFrameworkCore; 
+namespace HotelesBeachSABackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FacturaController : ControllerBase
+    {
+        private readonly DbContextHotelBeachSA _context = null; 
+
+        public FacturaController(DbContextHotelBeachSA context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Listado")]
+        public async  Task<IActionResult> Listado() {
+            try
+            {
+                var list = await _context.Facturas.ToListAsync();
+
+                if (list.Count == 0 || list == null)
+                {
+                    return NotFound("No hay facturas registradas.");
+                }
+
+                return Ok(list);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new
+                {
+                    message = $"Ocurrió un error al obtener la lista de facturas habilitadas",
+                    detalle = ex.Message
+                });
+            }
+        }
+
+    }
+}
--- HotelesBeachSABackend/obj/Debug/net8.0/HotelesBeachSABackend.AssemblyInfo.cs
+++ HotelesBeachSABackend/obj/Debug/net8.0/HotelesBeachSABackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelesBeachSABackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da980a7b6cdfb4e793447a4fefff7d69a1e716e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae9e595071d3a4195dead42771457500042dd06")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelesBeachSABackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelesBeachSABackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
