b85ffaea19bce0773b35b7b953db0eae35263f69
cap1112 <angelv.salazar11@gmail.com>
2024-11-22 22:39:20 -0600

    feat: email sender

---
+++ HotelesBeachSABackend/Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using HotelesBeachSABackend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelesBeachSABackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+
+
+
+        [HttpPost("Send")]
+        public async Task<IActionResult> Send(string email, string theme, string body)
+        {
+            await emailService.SendEmail(email, theme, body);
+            return Ok();
+        }
+
+    }
+}
---
+++ HotelesBeachSABackend/Services/EmailService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using System.Net.Mail;
+
+namespace HotelesBeachSABackend.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmail(string emailRecipient, string tema, string cuerpo);
+    }
+
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration configuration;
+        public EmailService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+        public async Task SendEmail(string emailRecipient, string tema, string cuerpo)
+        {
+            var emailSender = configuration.GetValue<string>("EMAIL_CONFIGURATIONS:EMAIL");
+            var password = configuration.GetValue<string>("EMAIL_CONFIGURATIONS:PASSWORD");
+            var host = configuration.GetValue<string>("EMAIL_CONFIGURATIONS:HOST");
+            var port = configuration.GetValue<int>("EMAIL_CONFIGURATIONS:PORT");
+
+            var smptClient = new SmtpClient(host, port);
+            smptClient.EnableSsl = true;
+            smptClient.UseDefaultCredentials = false;
+            smptClient.Credentials = new NetworkCredential(emailSender, password);
+
+            var message = new MailMessage(emailSender!, emailRecipient, tema, cuerpo);
+            await smptClient.SendMailAsync(message);
+        }
+
+    }
+}
