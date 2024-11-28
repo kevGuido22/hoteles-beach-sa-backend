661ecd24c0ec2dbb3fc50761e9ae987797dfe39d
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-11-28 00:19:05 -0600

    feat: RolPermisoDTO model added

---
+++ HotelesBeachSABackend/Models/Custom/RolPermisoDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace HotelesBeachSABackend.Models.Custom
+{
+    public class RolPermisoDTO
+    {
+        public int RolId { get; set; }
+
+        public string RolName { get; set; }
+
+        public int PermisoId { get; set; }
+
+        public string PermisoName { get; set; }
+    }
+}
