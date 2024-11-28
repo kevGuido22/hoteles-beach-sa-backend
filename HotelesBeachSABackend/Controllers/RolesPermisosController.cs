4ec3e9f2e5ea3532dc362cb56e61ba386c425a0e
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-11-28 00:19:17 -0600

    feat: RolesPermisosController adde

---
+++ HotelesBeachSABackend/Controllers/RolesPermisosController.cs
@@ -0,0 +1,42 @@
+﻿using HotelesBeachSABackend.Data;
+using HotelesBeachSABackend.Models;
+using HotelesBeachSABackend.Models.Custom;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelesBeachSABackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesPermisosController : Controller
+    {
+
+        private readonly DbContextHotelBeachSA _context = null;
+
+        public RolesPermisosController(DbContextHotelBeachSA context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Listado")]
+        public async Task<IActionResult> Listado()
+        {
+            var rolesPermisos = await _context.RolesPermisos
+       .Include(rp => rp.Rol)          // Incluye la información del Rol
+       .Include(rp => rp.Permiso)      // Incluye la información del Permiso
+       .Select(rp => new RolPermisoDTO // Proyecta los datos al DTO
+       {
+           RolId = rp.RolId,
+           RolName = rp.Rol.Name,     // Asumiendo que la propiedad se llama "Nombre"
+           PermisoId = rp.PermisoId,
+           PermisoName = rp.Permiso.Name // Asumiendo que la propiedad se llama "Nombre"
+       })
+       .ToListAsync();
+
+
+
+            return StatusCode(200, rolesPermisos);
+        }
+
+    }
+}
