9cbe5ac4548540c098aa97dd512ce5140e517656
Kevin Guido Urbina <guidourbinakevin@gmail.com>
2024-11-28 01:50:14 -0600

    feat: UsuariosRolesController added

---
+++ HotelesBeachSABackend/Controllers/UsuariosRolesController.cs
@@ -0,0 +1,38 @@
+﻿using HotelesBeachSABackend.Data;
+using HotelesBeachSABackend.Models.Custom;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelesBeachSABackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosRolesController : Controller
+    {
+        private readonly DbContextHotelBeachSA _context = null;
+        public UsuariosRolesController(DbContextHotelBeachSA context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Listado")]
+        public async Task<IActionResult> Listado() 
+        {
+            var usuariosRoles = await _context.UsuariosRoles
+                .Include(rp => rp.Usuario)
+                .Include(rp => rp.Rol)
+                .Select(rp => new UsuarioRolDTO 
+                { 
+                    UsuarioRolID = rp.Id,
+                    UsuarioId = rp.Usuario.Id,
+                    Email = rp.Usuario.Email,
+                    NombreCompleto = rp.Usuario.Nombre_Completo,
+                    RolId = rp.Rol.Id,
+                    RolName = rp.Rol.Name
+                })
+                .ToListAsync();
+
+            return StatusCode(200, usuariosRoles);
+        }
+    }
+}
